0c7cf0683676a66ef3401953bec221ba398dddcf
ArtemLevin <python_study_2022@mail.ru>
2022-12-01 23:35:18 +0300

    One more fix

--- TheGame/Program.cs
+++ TheGame/Program.cs
@@ -1,22 +1,21 @@
 ﻿Console.Write("Введите общее число печенек N:  ");
-int N = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine()!);
 Console.Write("Введите максимальное значение печенек для взятия игроком: ");
-int m = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine()!);
 
 int y; //остаток после взятия
 int x; // первое взятие игроком 1
 int k; // взятие игроком 2
-int count; // счетчик шагов
 
 
-x = N % (m+1);
+x = N % (m + 1);
 Console.Write("Первый игрок взял: " + x);
 Console.WriteLine(" ");
 y = N - x;
 Console.Write("Осталось после взятия первым игроком: " + y);
 Console.WriteLine(" ");
 
-while (y >0)
+while (y > 0)
 {
     k = new Random().Next(1, m); // рандомизируем взятие игроком 2
     Console.Write("Второй игрок взял: " + k);
@@ -24,7 +23,7 @@ while (y >0)
     y = y - k; // остаток от взятия игроком 2
     Console.Write("Осталось после взятия вторым игроком: " + y);
     Console.WriteLine(" ");
-        if (y == 0)
+    if (y == 0)
     {
         Console.Write("Победил игрок 2");
         Console.WriteLine(" ");
