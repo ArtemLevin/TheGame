f07910ec85f99ba9453d810703563adcbaf9f020
ArtemLevin <python_study_2022@mail.ru>
2022-12-02 03:23:57 +0300

    CreatingArray

---
+++ Arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = new int [10];
+
+int count = 0;
+
+while (count < array.Length)
+{
+    array[count] =new Random().Next(1, 10);
+    
+    Console.Write(array[count] + " " );
+    count ++;
+    
+}
+
